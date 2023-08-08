0f9482bacfd8b5c83af25243e5dfd48ef9239e37
Christopher Chase <1772922+DarkAxi0m@users.noreply.github.com>
2023-08-09 09:15:11 +0930

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,50 +1,60 @@
 #!/bin/bash
 
+
+
 interface_name=$(ip a | grep "^[0-9]:" | grep -v lo: | cut -d ":" -f2 | xargs)
-default_subnet_mask="255.255.255.0"
-default_gateway="10.1.1.1"
-default_dns="10.1.1.10"
+current_ip=$(ip -o -4 addr show dev $interface_name | awk '{split($4, a, "/"); print a[1]}')
 
 echo "interface detected: ${interface_name}"
+echo "current ip: ${current_ip}"
 
-read -p "Enter the desired IP address: " ip_address
-read -p "Enter subnet mask [$default_subnet_mask]: " subnet_mask
-subnet_mask=${subnet_mask:-$default_subnet_mask}
-
-read -p "Enter gateway [$default_gateway]: " gateway
-gateway=${gateway:-$default_gateway}
-
-read -p "Enter DNS server 1 [$default_dns]: " dns1
-dns1=${dns1:-$default_dns}
-
-
+read -p "Do you want to set up a static IP address? (y/n): " setup_static_ip
 read -p "Do you want to install Docker? (y/n): " install_docker
 
-# Backup the original network configuration file
-sudo cp /etc/network/interfaces /etc/network/interfaces.bak
-
-# Create a new network configuration file
-sudo tee /etc/network/interfaces > /dev/null << EOL
-source /etc/network/interfaces.d/*
-
-auto lo
-iface lo inet loopback
-
-# The primary network interface
-iface $interface_name inet6 auto
-
-auto $interface_name
-iface $interface_name inet static
-    address $ip_address
-    netmask $subnet_mask
-    gateway $gateway
-    dns-nameservers $dns1
+if [ "$setup_static_ip" == "y" ]; then
+    default_subnet_mask="255.255.255.0"
+    default_gateway="10.1.1.1"
+    default_dns="10.1.1.10"
+    read -p "Enter the desired IP address: " ip_address
+    read -p "Enter subnet mask [$default_subnet_mask]: " subnet_mask
+    subnet_mask=${subnet_mask:-$default_subnet_mask}
+    
+    read -p "Enter gateway [$default_gateway]: " gateway
+    gateway=${gateway:-$default_gateway}
+    
+    read -p "Enter DNS server 1 [$default_dns]: " dns1
+    dns1=${dns1:-$default_dns}
+    
+    
+    # Backup the original network configuration file
+    sudo cp /etc/network/interfaces /etc/network/interfaces.bak
+    
+    # Create a new network configuration file
+    sudo tee /etc/network/interfaces > /dev/null << EOL
+    source /etc/network/interfaces.d/*
+    
+    auto lo
+    iface lo inet loopback
+    
+    # The primary network interface
+    iface $interface_name inet6 auto
+    
+    auto $interface_name
+    iface $interface_name inet static
+        address $ip_address
+        netmask $subnet_mask
+        gateway $gateway
+        dns-nameservers $dns1
 EOL
+    
+    # Restart the networking service to apply the changes
+    sudo service networking restart
 
-# Restart the networking service to apply the changes
-sudo service networking restart
-ip address
-echo "Static IP configuration has been applied."
+    echo "Static IP configuration has been applied."
+else
+    echo "Static IP setup was skipped."
+fi
+
 
 # Prompt to install Docker
 
