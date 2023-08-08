6591c19d17dc1ae671188632d166fa194a14df76
Christopher Chase <1772922+DarkAxi0m@users.noreply.github.com>
2023-08-09 09:02:33 +0930

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Guess the interface name (assuming there's only one non-loopback interface)
 interface_name=$(ip -o link show | awk -F': ' '{if ($2 != "lo") print $2; exit}')
+default_subnet_mask="255.255.255.0"
+default_gateway="10.1.1.1"
+default_dns="10.1.1.10"
 
-# Prompt for network configuration details
+echo "interface detected: ${interface_name}"
+
 read -p "Enter the desired IP address: " ip_address
-read -p "Enter the subnet mask: " subnet_mask
-read -p "Enter the gateway address: " gateway
-read -p "Enter DNS server 1: " dns1
-read -p "Enter DNS server 2: " dns2
+read -p "Enter subnet mask [$default_subnet_mask]: " subnet_mask
+subnet_mask=${subnet_mask:-$default_subnet_mask}
+
+read -p "Enter gateway [$default_gateway]: " gateway
+gateway=${gateway:-$default_gateway}
+
+read -p "Enter DNS server 1 [$default_dns]: " dns1
+dns1=${dns1:-$default_dns}
+
+read -p "Enter DNS server 1 [$default_dns]: " dns1
+dns1=${dns1:-$default_dns}  # Use default if input is empty
 
 # Backup the original network configuration file
 sudo cp /etc/network/interfaces /etc/network/interfaces.bak
@@ -28,10 +38,10 @@ iface $interface_name inet static
     address $ip_address
     netmask $subnet_mask
     gateway $gateway
-    dns-nameservers $dns1 $dns2
+    dns-nameservers $dns1
 EOL
 
 # Restart the networking service to apply the changes
 sudo service networking restart
-
+ip address
 echo "Static IP configuration has been applied."
